bf3e998b1bf835df353d81cfeeb28dc89e06fe5f
David <davidjfinder@gmail.com>
2024-10-21 15:19:41 -0400

    gModified functions and added comment questioning whether piece_selection was necessary. Changed name of teleport to teleport when suggested. Deleted secret passage function

--- controller/movement_controller.py
+++ controller/movement_controller.py
@@ -1,8 +1,12 @@
 def MovementController(object):
     def move(self,player,location):
-        #Moves a player
-    def teleport(self,player,location):
-        #
-    def legal_moves(self,player):
+        #Used when a player moves
+        pass
+    def legal_moves(self):
         #Determines if your moves are legal.
-    #Handles Teleports and Moves
+        #Handles Teleports and Moves
+        pass
+    def teleport_when_suggested(self):
+        #Used when a player is "suggested" as the suspect and is moved to 
+        #the room the suggester is in. 
+        pass
