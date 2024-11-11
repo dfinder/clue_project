a620d46ec9a2cc578e73c48d87f4c6b11791965a
Qi Gao <gq54368973@gmail.com>
2024-11-11 00:34:44 -0800

    Update suggestion.py

--- gamestate/suggestion.py
+++ gamestate/suggestion.py
@@ -2,11 +2,20 @@ from weapons import Weapon
 from character import Character
 from player import Player
 from rooms import Room
+
 class Suggestion(GameStateObject):
     suggestor: Player
     weapon: Weapon
     accused: Character
     location: Room
+
     def __init__(player,weapon,accused):
-        pass 
+        self.suggestor = suggestor
+        self.weapon = weapon
+        self.accused = accused
+        self.location = location
+
+    def __repr__(self):
+        # print the suggestion
+        return f"Suggestion by {self.suggestor.name}: {self.accused.name} with {self.weapon.name} in {self.location.name}"
 
